ec14b67afad4bea6a4a196aece34e5310fb04318
Alex Grover <alexgrover@microsoft.com>
2023-07-19 11:00:22 +0100

    Changed makeblobs colder to http

--- ArchiveFunction/MakeBlobsColder.cs
+++ ArchiveFunction/MakeBlobsColder.cs
@@ -1,16 +1,21 @@
 using System;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
-using Microsoft.Extensions.Logging;
-using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
 
 namespace groveale
 {
     public class MakeBlobsColder
     {
         [FunctionName("MakeBlobsColder")]
-        public async Task Run([TimerTrigger("0 0 2 * * *")]TimerInfo myTimer, ILogger log)
+        //public async Task Run([TimerTrigger("0 0 2 * * *")]TimerInfo myTimer, ILogger log)
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
 
@@ -21,11 +26,15 @@ namespace groveale
 
                 // Cool blobs
                 await AzureBlobHelper.MoveBlobsToCoolTier(settings.StorageConnectionString);
+
+                // Return the active files count in response
+                return new OkObjectResult("Yay");
             }
             catch (Exception ex)
             {
                 // Return error in response
                 log.LogError(ex.Message);
+                return new BadRequestObjectResult($"Error in request: {ex.Message}");
             }
       
         }
